b64d9d5eb4f228231bf050c3987d221d71c167a9
Victor-Edward <victoredward30@gmail.com>
2021-03-25 11:00:31 -0300

    feat: Added navbar and some css styles

--- basic_app/models.py
+++ basic_app/models.py
@@ -4,12 +4,14 @@ class Customer(models.Model):
     name = models.CharField(max_length=100)
     contact = models.CharField(max_length=100)
     address = models.CharField(max_length=150)
+    
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     product_type = models.CharField(max_length=100)
     quantity = models.IntegerField()
+    
 
 
     
