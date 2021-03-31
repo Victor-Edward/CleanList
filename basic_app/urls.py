b21d849a62f8b63f58cddff2d10cac643894ff1e
Victor-Edward <victoredward30@gmail.com>
2021-03-31 09:20:58 -0300

    fix: Fixed wrong url redirect in urls.py

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from basic_app.views import Index, ProductDetailView, CreateProductView
+from basic_app.views import (Index, ProductDetailView, CreateProductView,
+                            ProductListView)
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-    path('products/', ProductDetailView.as_view(), name='product_view'),
+    path('products/', ProductListView.as_view(), name='product_view'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('products/novo/', CreateProductView.as_view(), name='product_create'),
 
